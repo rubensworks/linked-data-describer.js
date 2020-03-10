34b2dd7e70f5cd662e7be0aac45395b8d3f786a9
Ruben Taelman <ruben.taelman@ugent.be>
2020-03-10 10:55:58 +0100

    Allow a config file to be passed

--- bin/server.ts
+++ bin/server.ts
@@ -1,7 +1,18 @@
 #!/usr/bin/env node
 import {newEngine} from "@comunica/actor-init-sparql";
+import {readFileSync} from "fs";
+import {join} from "path";
 import {Server} from "../";
 
-const context = { sources: ['https://data.linkeddatafragments.org/eswc2020'] };
+// Validate args
+if (process.argv.length !== 3) {
+  process.stderr.write(`Missing config file.\n`);
+  process.stderr.write(`Usage:\n`);
+  process.stderr.write(`  linked-data-describer config.json\n`);
+  process.exit(1);
+}
+
+// Start server
+const context = JSON.parse(readFileSync(join(process.cwd(), process.argv[2]), 'utf8'));
 const server = new Server(newEngine(), context);
-server.start(8080);
+server.start(context.port || 8080);
