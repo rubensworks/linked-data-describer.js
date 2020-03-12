6d66cb51710df649f6272d461a574b4a8b23c42d
Ruben Taelman <ruben.taelman@ugent.be>
2020-03-12 11:18:13 +0100

    Add unit tests for basic functions

---
+++ test/Server-test.ts
@@ -0,0 +1,41 @@
+import {Server} from "../";
+
+describe('Server', () => {
+
+  const server: Server = new Server(null, {
+    baseIRI: 'http://example.org/',
+    cacheDuration: 10,
+    title: 'My Server',
+  });
+
+  describe('#getHeaders', () => {
+
+    it('should handle text media types', async () => {
+      expect(server.getHeaders('text/html')).toEqual({
+        'Content-Type': 'text/html;charset=utf-8',
+      });
+    });
+
+    it('should handle other media types', async () => {
+      expect(server.getHeaders('application/ld+json')).toEqual({
+        'Content-Type': 'application/ld+json',
+      });
+    });
+
+  });
+
+  describe('#getQuery', () => {
+
+    it('should produce a CONSTRUCT query for a given path based on the baseIRI', async () => {
+      expect(server.getQuery('my/path')).toEqual(`
+CONSTRUCT {
+  ?s ?p ?o
+} WHERE {
+  ?s ?p ?o.
+  FILTER(regex(str(?s), "^http://example.org/my/path" ) )
+}`);
+    });
+
+  });
+
+});
